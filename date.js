488fc0ac7f7ba014344cabbb92b64198bb96c2eb
code by HImanshu <himanshuanand412gmail.com>
2022-11-12 21:45:04 +0530

    initial commit

---
+++ date.js
@@ -0,0 +1,23 @@
+
+exports.getdate =  function() {
+    const today = new Date();
+
+    const option = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+        year: "numeric"
+    }
+
+    return  today.toLocaleDateString("en-US", option);
+}
+
+exports.getday = function(){
+    const today = new Date();
+
+    const option = {
+        weekday: 'long'
+    }
+
+    return  today.toLocaleDateString("en-US", option);
+}
